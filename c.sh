21fe93f23249d3410aeae66e80a86101973e9f0d
nt <>
2023-10-25 19:37:31 +0300

    initial d

---
+++ c.sh
@@ -0,0 +1,8 @@
+rm -rfv CMakeCache.txt
+rm -rfv CMakeFiles
+rm -rfv cmake_install.cmake
+rm -rfv compile_commands.json
+rm -rfv Makefile
+rm -rfv capirinha
+rm -rfv res/META-INF/com/google/android/update-binary
+rm -rfv update.zip
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
